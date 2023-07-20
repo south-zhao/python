1b44a4598fe98eaa609c1c13360df4656c0b4a4e
south-zhao <2252585312@qq.com>
2023-07-20 09:56:34 +0800

    数学建模

--- 建模/sympy1.py
+++ 建模/sympy1.py
@@ -8,10 +8,8 @@
     -*- coding: utf-8 -*-
 """
 import sympy as sp
-x1, x2 = sp.symbols('x1,x2')
-x0 = sp.solve([x1**2+x2**2-1, x1-x2], [x1, x2])
-print(x0)
-
-
+# x1, x2 = sp.symbols('x1,x2')
+# x0 = sp.solve([x1**2+x2**2-1, x1-x2], [x1, x2])
+# print(x0)
 
 
